1603146d30230690877b55a935bed3b0bcabd7dc
Conf8house <rin_ruger@mail.ru>
2017-06-22 23:55:11 +1000

    Improove lab.

--- PasswordHyding/Form1.cs
+++ PasswordHyding/Form1.cs
@@ -21,13 +21,23 @@ namespace WindowsFormsApplication1
         {
             this.Text = "Password hyding";
             label1.Text = null;
-            textBox1.Clear();
+            textBox1.Clear(); textBox1.TabIndex = 0;
             textBox1.PasswordChar = '*';
+            textBox1.Font = new Font("Courier New", 10.0F);
+            //or textBox1.Font = new Font(
+            //      FontFamily.GenericMonospace, 10.0F);
             textBox1.TextAlign = HorizontalAlignment.Center;
+            button1.Text = "Password";
 
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+            //label1.Text = textBox1.Text;
+            label1.Font = new Font("Courier New", 15.0F, FontStyle.Bold);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
             label1.Text = textBox1.Text;
         }
